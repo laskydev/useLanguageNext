22120c8ab140f084f063b92d5ee7b9dba03d869b
Eduardo <eduardo@waof.co>
2022-12-13 22:22:33 -0600

    Put in index

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-import { useLanguage } from './useLanguage'
-export { useLanguage} from './useLanguage'
+import { useRouter } from 'next/router'
+
+export const useLanguage = () => {
+  const { locale } = useRouter()
+  const handleLanguage = (handlerLanguage) => {
+    return handlerLanguage[locale]
+  }
+
+  return { handleLanguage }
+}
+
 export default useLanguage
