f99f9475477636a738b696dd75a815c268e9abf4
Eduardo Fortuna <eduardo@waof.co>
2022-12-26 15:22:19 -0600

    Add test sample and jest

---
+++ tests/index.test.js
@@ -0,0 +1,8 @@
+//Its not working now because of the error in the imports of React and Next.js 
+//I'm trying to fix it
+
+// sample test empty
+test('sample test', () => {
+    expect(true).toBe(true);
+    }
+);
